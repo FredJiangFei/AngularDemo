6d03427ee1664c00209dc69d9413254ed708892f
FredJiang <jiang.fei@ffcg.se>
2018-08-08 10:38:50 +0800

    bind event

---
+++ bind-demo/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Product } from './models/product';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  product: Product;
+
+  addToCartHandler(product: Product) {
+    this.product = product;
+  }
+}
---
+++ bind-demo/src/app/product/product.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  product: Product
+
+  @Output()
+  addCart: EventEmitter<Product> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+    this.product = new Product('iphone7', 5000);
+
+    setInterval(() => {
+      this.product.price = 5000 * Math.random()
+    }, 1000);
+  }
+
+  addToCart() {
+    this.addCart.emit(new Product(this.product.name, this.product.price));
+  }
+}
