e91e1270f9af0d6e761beed76edf46f5e8db34a8
FredJiang <jiangf@shinetechchina.com>
2018-08-08 20:13:25 +0800

    DoCheck

--- life-cycle-demo/src/app/onchanges/onchanges.component.ts
+++ life-cycle-demo/src/app/onchanges/onchanges.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-onchanges',
   templateUrl: './onchanges.component.html',
   styleUrls: ['./onchanges.component.css']
 })
-export class OnchangesComponent implements OnInit, OnChanges {
+export class OnchangesComponent implements OnInit, OnChanges, DoCheck {
 
   @Input()
   greeting: string;
@@ -15,6 +15,10 @@ export class OnchangesComponent implements OnInit, OnChanges {
 
   message: string = 'ngOnChange';
 
+  oldName: string;
+  changeDetected: boolean = false;
+  noChangeCount: number;
+
   constructor() { }
 
   ngOnInit() {
@@ -22,5 +26,22 @@ export class OnchangesComponent implements OnInit, OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log(JSON.stringify(changes));
+  }
+
+  ngDoCheck(): void {
+    if (this.user.name != this.oldName) {
+      this.changeDetected = true;
+      console.log(`DoCheck user name change to ${this.user.name} from ${this.oldName}`);
+      this.oldName = this.user.name;
+    }
+
+    if (this.changeDetected) {
+      this.noChangeCount = 0;
+    } else {
+      this.noChangeCount ++;
+      console.log(`Docheck: name not change, but DoCheck was called ${this.noChangeCount} times.`)
+    }
+
+    this.changeDetected = false;
   }
 }
