fa2fcc609374934f1849293c48ebc4aa29b2c538
Fred <jiangf@shinetechchina.com>
2018-08-11 07:35:18 +0800

    mat working

---
+++ task-manager/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { ShareModule } from './share/share.module';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    ShareModule,
+    MatSidenavModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
