dd2b2de3a23f435a3fa587dc409f8400ce55b2dd
Fred <jiangf@shinetechchina.com>
2018-08-11 20:44:22 +0800

    login card

--- task-manager/src/app/core/core.module.ts
+++ task-manager/src/app/core/core.module.ts
@@ -1,5 +1,4 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
