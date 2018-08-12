f80d2730fccc3740bd38f1aa8965e68fd96a556d
FredJiang <jiangf@shinetechchina.com>
2018-08-12 16:15:07 +0800

    add register component

--- task-manager/src/app/app-routing.module.ts
+++ task-manager/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './login/register/register.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
