0e4a81042cc1aa8fed20de5b9b0c60e76e64f3f3
FredJiang <jiangf@shinetechchina.com>
2018-08-14 21:48:21 +0800

    animation use

--- task-manager/src/app/product/product-item/product-item.component.ts
+++ task-manager/src/app/product/product-item/product-item.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit, Output, Input } from '@angular/core';
+import { Component, OnInit, Output, Input, HostBinding, HostListener } from '@angular/core';
 import { Product } from '../../domain/product.domain';
+import { cardAmin } from '../../animates/card.animate';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.css']
+  styleUrls: ['./product-item.component.css'],
+  animations:[ cardAmin ]
 })
 export class ProductItemComponent implements OnInit {
   
   @Input()
   public item: Product;
+
+  @HostBinding('@card') cardState = 'out';
+
+  @HostListener('mouseenter')
+  onMouseEnter(){
+    this.cardState = 'hover';
+  }
+
+  @HostListener('mouseleave')
+  onmouseleave(){
+    this.cardState = 'out';
+  }
 
   constructor() { }
 
