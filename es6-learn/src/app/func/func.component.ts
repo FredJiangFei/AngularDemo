739d734c5e35b784e14ee693fc08584f32fada8c
FredJiang <jiangf@shinetechchina.com>
2018-08-28 07:22:49 +0800

    es6

--- es6-learn/src/app/func/func.component.ts
+++ es6-learn/src/app/func/func.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FuncComponent implements OnInit {
 
-  constructor() { }
+  id = 21;
+  foo() {
+    setTimeout(() => {
+      console.log('id', this.id);
+    }, 100);
+  }
+
+  s1 = 0;
+  s2 = 0;
 
   ngOnInit() {
+    //arrow
+    // this.foo.call({ id: 42 });
+
+    setInterval(() => this.s1++, 1000);
+
+    setInterval(function () {
+      this.s2++;
+    }, 1000);
+
+    setTimeout(() => {
+      console.log('s1:', this.s1);
+    }, 3100);
+
+    setTimeout(() => {
+      console.log('s2:', this.s2);
+    }, 3100);
   }
 
 }
