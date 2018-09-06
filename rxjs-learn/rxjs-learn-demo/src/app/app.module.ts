29c9088c8a5ad2501c2148ba9edf8799fcbd200b
FredJiang <jiang.fei@ffcg.se>
2018-09-06 14:43:59 +0800

    auto complete

--- rxjs-learn/rxjs-learn-demo/src/app/app.module.ts
+++ rxjs-learn/rxjs-learn-demo/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SimpleDragComponent } from './simple-drag/simple-drag.component';
 import { SimpleTestComponent } from './simple-test/simple-test.component';
 import { MouseComponent } from './mouse/mouse.component';
+import { AutoCompleteComponent } from './auto-complete/auto-complete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SimpleDragComponent,
     SimpleTestComponent,
-    MouseComponent
+    MouseComponent,
+    AutoCompleteComponent
   ],
   imports: [
     BrowserModule
