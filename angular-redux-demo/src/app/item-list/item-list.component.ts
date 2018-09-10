7fda79846ba3b1fdcffc14e4829f0627df73bd9a
FredJiang <jiangf@shinetechchina.com>
2018-09-11 06:36:35 +0800

    add filter

--- angular-redux-demo/src/app/item-list/item-list.component.ts
+++ angular-redux-demo/src/app/item-list/item-list.component.ts
@@ -1,8 +1,10 @@
+import { ShowAll, ShowActive, ShowComplete } from './../actions/todo.filter.actions';
 import { AddTodo, RemoveTodo, ToggleTodo } from './../actions/todo.actions';
 import { Item } from './../models/item';
 import { Component, OnInit } from '@angular/core';
 import { ItemService } from '../services/item.service';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
+import { startWith } from 'rxjs/operators';
 import { Store, select } from '@ngrx/store';
 
 @Component({
@@ -12,21 +14,28 @@ import { Store, select } from '@ngrx/store';
 })
 export class ItemListComponent implements OnInit {
   items: Observable<Item[]>;
-  constructor(
-    private itemService: ItemService,
-    private store$: Store<Item>) { }
+  ids = [1, 2, 3, 4, 5];
+
+  constructor(private store$: Store<Item>) { }
 
   ngOnInit() {
-     this.items = this.store$.pipe(select('todos'));
+     const data$ = this.store$.pipe(select('todos'), startWith([]));
+     const filterData$ = this.store$.pipe(select('todoFilter'));
+     this.items = combineLatest(data$, filterData$,
+      (todos: Item[], filter: any) => todos.filter(filter));
   }
 
-  addTodo(desc: string) {
+
+
+  addTodo() {
     const todoToAdd = {
-      id: 1,
-      name: 'item 1',
+      id: this.ids[0],
+      name: 'item ' + this.ids[0],
       description: 'This is description'
     };
     this.store$.dispatch(new AddTodo(todoToAdd));
+    const curId = this.ids[0];
+    this.ids = this.ids.filter(id => id !== curId);
   }
 
   removeTodo(item: Item) {
@@ -35,5 +44,17 @@ export class ItemListComponent implements OnInit {
 
   toggleTodo(item: Item) {
     this.store$.dispatch(new ToggleTodo(item));
+  }
+
+  all() {
+    this.store$.dispatch(new ShowAll());
+  }
+
+  completed() {
+    this.store$.dispatch(new ShowComplete());
+  }
+
+  actived() {
+    this.store$.dispatch(new ShowActive());
   }
 }
--- angular-redux-demo/src/app/reducers/todo.filter.reducer.ts
+++ angular-redux-demo/src/app/reducers/todo.filter.reducer.ts
@@ -1,13 +1,15 @@
+import { TodoFilterAction, TodoFilterActionTypes } from './../actions/todo.filter.actions';
+import { Item } from '../models/item';
 
-
-export const todoFilterReducer = (state = [], {type, payload} ) => {
-    switch (type) {
-        case 'ADD_TODO':
-            return [...state, payload];
-        default: {
-            return state;
-        }
+export const todoFilterReducer = (state = (todo: Item) => todo, action: TodoFilterAction) => {
+    switch (action.type) {
+      case TodoFilterActionTypes.SHOW_ALL:
+        return todo => todo;
+      case TodoFilterActionTypes.SHOW_ACTIVE:
+        return todo => !todo.completed;
+      case TodoFilterActionTypes.SHOW_COMPLETED:
+        return todo => todo.completed;
+      default:
+        return state;
     }
-};
-
-
+  };
