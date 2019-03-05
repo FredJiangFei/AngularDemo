37bb95702e03bd48e20780b894478740e3e636b3
FredJiang <jiangf@shinetechchina.com>
2019-03-05 13:47:23 +0800

    remove demo

--- router-demo/src/app/guard/unsave.guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanDeactivate } from "@angular/router";
-import { ProductsComponent } from "../products/products.component";
-
-export class UnsavedGuard implements CanDeactivate<ProductsComponent>{
-
-    canDeactivate(component: ProductsComponent) {
-        return window.confirm('not saved');
-    }
-}
--- server/node.server.ts
+++
@@ -1,7 +0,0 @@
-import * as http from 'http'
-
-const server = http.createServer((req, res) => {
-    res.end('Helle node');
-});
-
-server.listen(8000);
